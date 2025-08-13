fb2018f3e79c2db00dde76f9ba1f55e2607535b3
priyavaratsaini3 <priyavartsaini3@gmail.com>
2025-08-13 07:18:23 +0530

    updated

--- Two_pointer/moves_zeros.py
+++ Two_pointer/moves_zeros.py
@@ -10,23 +10,3 @@ while left < len(nums):
      nums[left] = 0
      left += 1
 print(nums)
-# def move_zeroes(nums):
-#     # Pointer for position of the next non-zero element
-#     insert_pos = 0
-    
-#     # First pass: move all non-zero elements to the front
-#     for num in nums:
-#         if num != 0:
-#             nums[insert_pos] = num
-#             insert_pos += 1
-    
-#     # Second pass: fill the rest with zeros
-#     while insert_pos < len(nums):
-#         nums[insert_pos] = 0
-#         insert_pos += 1
-
-
-# # Example
-# nums = [0, 5, 3, 0, 7, 9]
-# move_zeroes(nums)
-# print(nums)  # [5, 3, 7, 9, 0, 0]
