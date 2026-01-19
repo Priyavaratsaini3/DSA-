fecc5a876536d6e2e6131f4cadc355207422e0c1
priyavaratsaini3 <priyavartsaini3@gmail.com>
2026-01-19 16:40:12 +0530

    creating a stack that has the function are push,length, peel, pop

---
+++ Stack.py
@@ -0,0 +1,31 @@
+class Stack:
+    def __init__(self):
+        self.s = []
+
+    def length(self):
+        return len(self.s)
+    
+    def push(self,value):
+        self.s.insert(0,value)
+    
+    def peek(self):
+        if len(self.s) == 0:
+            raise Exception("Stack is Empty")
+        else:
+            return self.s[0]
+    
+    def pop(self):
+        if len(self.s) == 0:
+            raise Exception("Stack is Empty")
+        else:
+            return self.s.pop(0)
+        
+stk = Stack()
+stk.push(10)
+stk.push(44)
+stk.push(412)
+stk.push(4122)
+stk.push(413)
+stk.pop()
+print(stk.length())
+print(stk.peek())
